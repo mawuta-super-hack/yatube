d125915981b075ec4cb86f967a3a6c25af729d46
Мария Клименкова <klimenkovama@gmail.com>
2022-06-15 00:09:52 +0500

    -add_com

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -72,7 +72,7 @@ class PostsPagesTests(TestCase):
         self.user = User.objects.get(username='auth')
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
-        
+        cache.clear()
 
     def test_pages_coorrect(self):
         '''Проверка шаблона'''
@@ -247,7 +247,6 @@ class PaginatorViewTest(TestCase):
         self.user = User.objects.get(username='noname')
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
-        
 
     def test_first_page_ten(self):
         '''Проверка количества постов на первой странице'''
@@ -295,7 +294,6 @@ class CacheTest(TestCase):
         self.user = User.objects.get(username='new')
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
-        
 
     def test_content_cache(self):
         '''Проверка кэша на главной странице'''
